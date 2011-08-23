62e4022cc1d5bac92eb57cef2cd917e62d21ad6c
Štěpán Mík <a.b.c@d.e.f.com>
2011-08-23 15:29:54 +0200

    Compare menu now works.

--- src/org/twinstone/eclipse/projectcompare/handlers/DefaultCounterpartAction.java
+++ src/org/twinstone/eclipse/projectcompare/handlers/DefaultCounterpartAction.java
@@ -1,15 +1,21 @@
 package org.twinstone.eclipse.projectcompare.handlers;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.mapping.ResourceMapping;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
-import org.twinstone.eclipse.projectcompare.ui.SynchronizeResourceTreeDialog;
+import org.eclipse.ui.ide.ResourceUtil;
+import org.twinstone.eclipse.projectcompare.Activator;
 
 public class DefaultCounterpartAction implements IObjectActionDelegate {
 
 	private IWorkbenchPart targetPart;
+	private ResourceMapping mapping;
 	
 	public DefaultCounterpartAction() {
 		/**/
@@ -18,20 +24,31 @@ public class DefaultCounterpartAction implements IObjectActionDelegate {
 	@Override
 	public void run(IAction action) {
 		Shell shell = targetPart.getSite().getShell();
-		SynchronizeResourceTreeDialog sd = new SynchronizeResourceTreeDialog(shell, null, null);
-		sd.open();
+		IFile file = ResourceUtil.getFile(mapping);
+		if (file==null) return;
+		try {
+			new PerformCompare(file, shell).run();
+		} catch (CoreException e) {
+			Activator.getDefault().getLog().log(e.getStatus());
+		} 
 	}
 
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-
+		mapping = null;
+		if (!(selection instanceof IStructuredSelection)) return;
+		IStructuredSelection ss = (IStructuredSelection) selection;
+		if (ss.size()!=1) return;
+		Object o = ss.getFirstElement();
+		if (!(o instanceof ResourceMapping)) return;
+		mapping = (ResourceMapping) o;
 	}
 
 	@Override
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
 		this.targetPart = targetPart;
-
+		Object adapter = targetPart.getAdapter(ResourceMapping.class);
+		if (adapter!=null) mapping = (ResourceMapping) adapter;
 	}
 
 }
